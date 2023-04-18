3cc937f720a653a71d4b081715b494ca721a903d
Hanszman <victor.hanszman@hotmail.com>
2023-04-18 19:17:01 -0300

    correção na exibição da porta no console

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const express = require('express');
 const cors = require('cors');
 const api = express();
 const routes = require('./src/routes');
+const port = process.env.PORT || 3333;
 
 // Execution
 api.use(cors());
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
 api.use('/', routes);
-api.listen(process.env.PORT || 3333);
-console.log('Run API Express');
+api.listen(port);
+console.log('Run API Express on Port: ' + port);
